b27739407e1ee098e4d140894bbaf5d2644c5831
pkprabhat <pkc.prabhat@gmail.com>
2016-02-16 09:34:34 +0530

    Added Files on neuralnet

---
+++ Deep Learning Analysis/airbCCFunction.R
@@ -0,0 +1,39 @@
+library("neuralnet")
+set.seed(10000)
+
+testdata <- as.matrix(sample(seq(0.000,0.200,length=100),100,replace=FALSE),ncol=1)
+pred <- compute(fit,testdata)
+
+EAD = 200000
+LGD = 0.5
+R = 0.04
+
+x = 0:100
+#PD = 0.000 to 0.200
+M = 2
+
+eq0 <- function(x) {x/500}
+eq3 <- function(PD) {(LGD*pnorm(((1/(1-R))^0.5)*qnorm(PD)+((R/(1-R))^0.5)*qnorm(0.999))-LGD*PD)}
+eq4 <- function(K) {K*12.5*EAD}
+
+
+PD <- data.frame(testdata)
+colnames(PD)<- c("pd")
+
+for (i in 1:100){
+  K[i] = eq3(PD$pd[i])
+  RWA[i] = eq4(K[i])
+}
+Ki = data.frame(k=K)
+
+fit<-neuralnet(response~attribute,
+               data=data,
+               hidden=c(10,10),
+               threshold = 0.01
+)
+
+testdata <- as.matrix(sample(seq(0.000,0.200,length=100),100,replace=FALSE),ncol=1)
+pred <- compute(fit,testdata)
+result <- cbind(testdata,pred$net.result,Ki)
+colnames(result) <- c("Attribute", "Prediction", "Actual")
+round(result,4)
---
+++ Deep Learning Analysis/squareFunction.r
@@ -0,0 +1,18 @@
+library("neurelnet")
+set.seed(2016)
+#attribute <- as.data.frame(sample(seq(-2,2,length=50),50,replace=FALSE),ncol=1)
+#response <- attribute^2
+#data <- cbind(attribute, response)
+#colnames(data) <- c("attribute","response")
+#head(data,15)
+fit<-neuralnet(response~attribute,
+               data=data,
+               hidden=c(3,3),
+               threshold = 0.01
+)
+
+testdata <- as.matrix(sample(seq(-2,2,length=10),10,replace=FALSE),ncol=1)
+pred <- compute(fit,testdata)
+result <- cbind(testdata,pred$net.result,testdata^2)
+colnames(result) <- c("Attribute", "Prediction", "Actual")
+round(result,4)
